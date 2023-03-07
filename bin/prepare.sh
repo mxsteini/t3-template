458fa3eeb3e294d41a1e1434a0749124c449479a
Michael Stein <info@michaelstein-itb.de>
2023-03-07 17:33:22 +0100

    add rollback v1 cleanup

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
-function checkTool() {
-    if ! command -v $1 &>/dev/null; then
-        echo "$1 could not be found"
-        exit_abnormal
-    fi
-    echo $1 found
-}
-
 function insertVariables() {
     for key in "${!PREPARE[@]}"; do
         DATA=${PREPARE[${key}]}
         if [[ ! -z ${DATA} ]]; then
             ESCAPED_REPLACE=$(printf '%s\n' "$DATA" | sed -e 's/[\/&]/\\&/g')
             sed -i -e "s/PREPARE_${key}/${ESCAPED_REPLACE}/g" $1
+        fi
+    done
+}
+
+function removeVariables() {
+    for key in "${!PREPARE[@]}"; do
+        DATA=${PREPARE[${key}]}
+        if [[ ! -z ${DATA} ]]; then
+            ESCAPED_REPLACE=$(printf '%s\n' "$DATA" | sed -e 's/[\/&]/\\&/g')
+            sed -i -e "s/${ESCAPED_REPLACE}/PREPARE_${key}/g" $1
         fi
     done
 }
@@ -27,35 +29,68 @@ function exit_abnormal() { # Function: Exit with error.
 declare -A PREPARE
 source ./bin/config.sh
 
-insertVariables ./.gitlab-ci.yml
-insertVariables ./bin/ci-deploy.sh
-insertVariables ./bin/down_sync.sh
-
-if [ -x ./config/sites/PREPARE_projectname ]; then
-    mv ./.ddev/config.yaml.dist ./.ddev/config.yaml
+if [ -z $1 ]; then
+    command=""
+else
+    command="$1"
 fi
-insertVariables ./.ddev/config.yaml
 
-if [ -d ./config/sites/PREPARE_projectname ]; then
-    mv ./config/sites/PREPARE_projectname ./config/sites/${PREPARE[projectname]}
-fi
-insertVariables ./config/sites/${PREPARE[projectname]}/config.yaml
+if [ "$command" == 'rollback' ]; then
+    removeVariables ./.gitlab-ci.yml
+    removeVariables ./bin/ci-deploy.sh
+    removeVariables ./bin/down_sync.sh
 
-if [ -d ./packages/PREPARE_LOWERVENDOR_site ]; then
-    mv ./packages/PREPARE_LOWERVENDOR_site ./packages/${PREPARE[LOWERVENDOR]}_site
-fi
-for f in $(find ./packages/${PREPARE[LOWERVENDOR]}_site -type f); do
-    insertVariables $f
-done
+    removeVariables ./.ddev/config.yaml
+    mv ./.ddev/config.yaml ./.ddev/config.yaml.dist
 
-if [ -d ./src/PREPARE_LOWERVENDOR_site ]; then
-    mv ./src/PREPARE_LOWERVENDOR_site ./src/${PREPARE[LOWERVENDOR]}_site
-fi
-for f in $(find ./src/${PREPARE[LOWERVENDOR]}_site -type f); do
-    insertVariables $f
-done
+    removeVariables ./config/sites/${PREPARE[projectname]}/config.yaml
+    mv ./config/sites/${PREPARE[projectname]} ./config/sites/PREPARE_projectname
 
-if [[ ! -x ./.env ]]; then
-    cp .env.dist .env
+    for f in $(find ./packages/${PREPARE[LOWERVENDOR]}_site -type f); do
+        removeVariables $f
+    done
+    mv ./packages/${PREPARE[LOWERVENDOR]}_site ./packages/PREPARE_LOWERVENDOR_site
+
+    for f in $(find ./src/${PREPARE[LOWERVENDOR]}_site -type f); do
+        removeVariables $f
+    done
+    mv ./src/${PREPARE[LOWERVENDOR]}_site ./src/PREPARE_LOWERVENDOR_site
+
+    removeVariables .env
+    mv .env .env.dist
+
+else
+
+    insertVariables ./.gitlab-ci.yml
+    insertVariables ./bin/ci-deploy.sh
+    insertVariables ./bin/down_sync.sh
+
+    if [ -x ./config/sites/PREPARE_projectname ]; then
+        mv ./.ddev/config.yaml.dist ./.ddev/config.yaml
+    fi
+    insertVariables ./.ddev/config.yaml
+
+    if [ -d ./config/sites/PREPARE_projectname ]; then
+        mv ./config/sites/PREPARE_projectname ./config/sites/${PREPARE[projectname]}
+    fi
+    insertVariables ./config/sites/${PREPARE[projectname]}/config.yaml
+
+    if [ -d ./packages/PREPARE_LOWERVENDOR_site ]; then
+        mv ./packages/PREPARE_LOWERVENDOR_site ./packages/${PREPARE[LOWERVENDOR]}_site
+    fi
+    for f in $(find ./packages/${PREPARE[LOWERVENDOR]}_site -type f); do
+        insertVariables $f
+    done
+
+    if [ -d ./src/PREPARE_LOWERVENDOR_site ]; then
+        mv ./src/PREPARE_LOWERVENDOR_site ./src/${PREPARE[LOWERVENDOR]}_site
+    fi
+    for f in $(find ./src/${PREPARE[LOWERVENDOR]}_site -type f); do
+        insertVariables $f
+    done
+
+    if [[ ! -x ./.env ]]; then
+        cp .env.dist .env
+    fi
+    insertVariables .env
 fi
-insertVariables .env
