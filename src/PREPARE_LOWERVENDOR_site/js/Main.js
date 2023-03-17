f3089c185782641bf84d117f96cecdc39fac1be0
Michael Stein <info@michaelstein-itb.de>
2023-03-17 13:41:56 +0100

    fix html and js

--- src/PREPARE_LOWERVENDOR_site/js/Main.js
+++ src/PREPARE_LOWERVENDOR_site/js/Main.js
@@ -1,11 +1,9 @@
 import Alpine from 'alpinejs'
-import intersect from '@alpinejs/intersect'
 import collapse from '@alpinejs/collapse'
 
 window.Alpine = Alpine
 
 
-Alpine.plugin(intersect)
 Alpine.plugin(collapse)
 
 
